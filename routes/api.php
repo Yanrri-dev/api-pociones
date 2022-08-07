3b9bf17fba4bc3a235a9b6fb38aa52b8266faf22
nrojas <nrojas@netmetrix.cl>
2022-08-07 06:41:02 -0400

    feat: agrega autenticador JWT a la API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['jwt.verify']], function() {
+
+
+
+});
+
+Route::group([
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [ AuthController::class, 'login' ]);
+    Route::post('logout', [ AuthController::class,'logout' ]);
+    Route::post('register', [ AuthController::class, 'register' ]);
+    Route::post('refresh', [ AuthController::class,'refresh' ]);
+    Route::post('me', [ AuthController::class,'me' ]);
 });
